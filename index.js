7e2ddf747efd5bf366f2adccbe5f47f76a6b85a0
Petr Stepchenko <nitrinodev@gmail.com>
2015-03-04 17:19:40 +0700

    fix functions naming

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var _     = require('lodash');
-var map   = require('map-stream');
-var pp    = require('preprocess');
-var path  = require('path');
+var _         = require('lodash');
+var map       = require('map-stream');
+var preproc   = require('preprocess');
+var path      = require('path');
 
 module.exports = function (options) {
   var opts    = _.merge({}, options);
   var context = _.merge({}, process.env, opts.context);
 
-  function ppStream(file, callback) {
+  function abReplace(file, callback) {
     var contents, extension;
 
     if (file.isNull()) return callback(null, file);
@@ -20,13 +20,13 @@ module.exports = function (options) {
     extension = _.isEmpty(opts.extension) ? getExtension(context.src) : opts.extension;
 
     contents = file.contents.toString('utf8');
-    contents = pp.preprocess(contents, context, extension);
+    contents = preproc.preprocess(contents, context, extension);
     file.contents = new Buffer(contents);
 
     callback(null, file);
   }
 
-  return map(ppStream);
+  return map(abReplace);
 };
 
 function getExtension(filename) {
